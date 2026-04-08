68b282782f1dcbcc964784965f5a502a3d394365
vinita0725 <vinita.725.singh@gmail.com>
2026-04-08 11:10:01 +0000

    datatypes comparision

---
+++ 01_basics/03_comparision.js
@@ -0,0 +1,16 @@
+console.log("2"==2)
+
+// console.log(null > 0);
+// console.log(null < 0);
+// console.log(null == 0);
+// console.log(null >= 0); //cuz comparision converts null to 0 and then comparres it
+// console.log(null <= 0); //but == does not convet it compares it as it is to a no that is 0
+
+// console.log(undefined > 0);
+// console.log(undefined < 0);
+// console.log(undefined == 0);
+// console.log(undefined >= 0);
+
+//strict check(===)
+
+console.log("2"===2)
