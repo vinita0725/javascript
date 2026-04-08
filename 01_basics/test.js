9ed2ae39d60ef22374de24532ce6638b791c517f
vinita0725 <vinita.725.singh@gmail.com>
2026-04-08 07:09:34 +0000

    datatypes

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,12 @@
 console.log("happy bday vini")
+
+console.log(typeof "vini")
+
+let age= 18
+console.log(typeof age)
+
+console.log(typeof 1)
+
+console.log(typeof null)
+
+console.log(typeof undefined)
