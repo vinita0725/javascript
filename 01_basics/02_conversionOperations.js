8af7f72d552c1ffcfcb7f16bee4ba36e865c6fe7
vinita0725 <vinita.725.singh@gmail.com>
2026-04-08 10:42:12 +0000

    conversion operator

---
+++ 01_basics/02_conversionOperations.js
@@ -0,0 +1,33 @@
+//OPERATIONS
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**3)
+// console.log(5/2)
+// console.log(5%2)
+
+let str1="hello"
+ let str2=" vini"
+
+ let str3= str1+str2
+// console.log(str3);
+let str4= 2
+//console.log(str1+str4)
+
+// strings can also be added but, if we add one string with a number it just pastes the no as string
+
+// console.log("1"+2);
+// console.log(1+"2"); 
+// console.log("1"+2+2); //till this works as string
+// console.log(2+2+"1"); //but this added the first two
+
+//special operation conversions
+// console.log(+true)
+// console.log(+"")
+
+let gameCounter= 100
+//gameCounter++ //postfix operator
+++gameCounter //prefix operator
+console.log(gameCounter)
+
+
