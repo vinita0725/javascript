d9307d265aeea551f53f06ff94701b0ea6b8065e
vinita0725 <vinita.725.singh@gmail.com>
2026-04-15 08:31:18 +0000

    compasision of datatypes

--- 01_basics/03_comparision.js
+++ 01_basics/03_comparision.js
@@ -1,16 +1,24 @@
-console.log("2"==2)
+//in comparison generally
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2==1);
+// console.log(2!=1);
 
-// console.log(null > 0);
+
+
+console.log("2"==2) //converts string to no while comparing , not recomended
+
+// console.log(null > 0); //this needs to be avaoided
 // console.log(null < 0);
 // console.log(null == 0);
 // console.log(null >= 0); //cuz comparision converts null to 0 and then comparres it
 // console.log(null <= 0); //but == does not convet it compares it as it is to a no that is 0
 
-// console.log(undefined > 0);
+// console.log(undefined > 0); // this need to be avoided
 // console.log(undefined < 0);
 // console.log(undefined == 0);
 // console.log(undefined >= 0);
 
 //strict check(===)
 
-console.log("2"===2)
+console.log("2"===2) //this time no conversion happens, it compares both datatype nad value
